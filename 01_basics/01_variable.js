d9a63a437b6555c809b77694296eb9412c3b78f1
Priyanshu Rawat <167407828+priyanshurawat1@users.noreply.github.com>
2024-04-19 07:47:28 +0000

    basics

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const accountId = 93945
+let accountEmail = "abcd@gmail.com"
+var accountPassword = "fgjhj"
+accountCity = "Delhi"// Possible but not likely to be declared like this
+let accountState
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
