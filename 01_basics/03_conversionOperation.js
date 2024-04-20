ce466b300e784394c2211093370266b9b2dc9a53
Priyanshu Rawat <167407828+priyanshurawat1@users.noreply.github.com>
2024-04-20 07:04:52 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let kills = "20+axsx"
+
+console.log(typeof kills)
+
+let valueInNumber = Number(kills)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let value = 20
+console.log(typeof value);
+
+let valueInstring = String(value)
+console.log(valueInstring);
+console.log(typeof valueInstring);
