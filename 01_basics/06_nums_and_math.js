c6306403763312214fc67dc8761f14d32816e682
Priyanshu Rawat <167407828+priyanshurawat1@users.noreply.github.com>
2024-04-27 16:15:55 +0000

    nums

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,11 @@
+const score = 95
+// console.log(score);
+
+const balance = new Number(200)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
