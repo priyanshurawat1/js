1dee0e4edfc0c9de09b19296d48eea3238bd92c5
Priyanshu Rawat <167407828+priyanshurawat1@users.noreply.github.com>
2024-04-27 14:28:31 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,28 @@
+const name = "Priyanshu"
+const repoCount = 1
+
+// console.log(name + repoCount);
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Assassian creed')
+// console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('s'));
+
+// console.log(gameName.substring(2,5));
+
+const newStringone = "  Priyanshu R   "
+// console.log(newStringone);
+// console.log(newStringone.trim());
+
+const newStringtwo = "Acer Nitro Sense & Cool Boost"
+console.log(newStringtwo.split(' '));
+console.log(newStringtwo.replace('&','@'));
+console.log(newStringtwo.includes('&'));
