621ab9d6277f140aa7394869cb62947c16ba8668
Priyanshu Rawat <167407828+priyanshurawat1@users.noreply.github.com>
2024-04-28 16:03:49 +0000

    nums and math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -9,3 +9,19 @@ const balance = new Number(200)
 
 const hundreds = 1000000
 // console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+
+// console.log(Math.random());
+// console.log(Math.random() * 10);
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
