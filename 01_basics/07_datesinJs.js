4f5e57689a4d6a5942499349386c948a461c2edd
Priyanshu Rawat <167407828+priyanshurawat1@users.noreply.github.com>
2024-04-29 16:09:14 +0000

    date

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,11 +1,16 @@
 let myDate = new Date()
-console.log(myDate);
+// console.log(myDate);
 
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toISOString());
-console.log(myDate.toJSON());
-console.log(myDate.toLocaleDateString());
-console.log(myDate.toLocaleString());
-console.log(myDate.toLocaleTimeString());
-console.log(myDate.toTimeString());
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+
+// let day = new Date(2024,7,5)
+// console.log(day.toDateString());
+let day = new Date(2024,7,5,8,30)
+console.log(day.toLocaleString());
