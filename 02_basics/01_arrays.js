2d82adad5b611d9ec3db82eab428f9101aa409c8
Priyanshu Rawat <167407828+priyanshurawat1@users.noreply.github.com>
2024-04-30 07:02:39 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,34 @@
+//Arrays
+
+const arr = [1,2,3,4,5]
+const carbrand = ['Pagani','Bugati','Porsche','Lamborgini']
+
+const arry = new Array(5,2,7,9,0)
+// console.log(arry);
+// console.log(arry[2]);
+
+
+// Array methods 
+
+
+const arr1 = [5,2,0,8,3,1]
+console.log(arr1);
+// arr1.push(7)
+// console.log(arr1)
+// arr1.pop()
+// console.log(arr1);
+
+// arr1.unshift(9)
+// console.log(arr1);
+// arr1.shift()
+// console.log(arr1);
+
+// slice, splice
+
+// const my1 = arr1.slice(1,3)
+// console.log(my1)
+// console.log(arr1);
+
+const my2 = arr1.splice(1,3)
+console.log(my2);
+console.log(arr1);
