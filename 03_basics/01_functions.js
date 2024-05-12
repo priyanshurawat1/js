227b1effe6f2c6f2b6df3587bda6f3972327c46b
Priyanshu Rawat <167407828+priyanshurawat1@users.noreply.github.com>
2024-05-12 06:50:48 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,20 @@
+function calculateCartPrice(...num){
+    return num
+}
+
+// console.log(calculateCartPrice(695,700,800));
+
+const user = {
+    username: "Priyanshu",
+    password: "5864123"
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and password is ${anyobject.password}`);
+}
+
+handleObject(user)
+handleObject({
+    username: "ArrowOG",
+    password: "7879515"
+})
