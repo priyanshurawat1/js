945bad7392111a897913e9eda5ee6c213c12820f
Priyanshu Rawat <167407828+priyanshurawat1@users.noreply.github.com>
2024-05-19 08:04:24 +0000

    iteration

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -1,5 +1,35 @@
 const coding = ["js",'ruby','java','python','c++']
 
 coding.forEach(function (val){
-    console.log(val);
+    // console.log(val);
 })
+
+// coding.forEach( (item) => console.log(item))
+
+function printMe(item) {
+    console.log(item);
+}
+// coding.forEach(printMe)
+
+// coding.forEach( (item,index,arr) => console.log(item,index,arr))
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "C++",
+        languageFileName: "cpp"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    }
+]
+
+myCoding.forEach( (item) => console.log(item.languageFileName))
