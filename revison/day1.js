996ef322cb4d3905f9cfe8c311be0df9d0eb3fd2
Priyanshu Rawat <167407828+priyanshurawat1@users.noreply.github.com>
2024-07-14 16:57:16 +0000

    rev day1

---
+++ revison/day1.js
@@ -0,0 +1,32 @@
+// var a = 55
+// console.log(a);
+
+// let nam = 'Priyanshu'
+// console.log(nam);
+
+// const value = true
+// console.log(value);
+
+
+// const num = 5
+// const str = 'string1'
+// const val = true
+// const obj1 ={
+//     name: 'abc',
+//     age: 21
+// }
+// const arr = [1,3,4,2]
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof val);
+// console.log(typeof obj1);
+// console.log(typeof arr);
+
+// let initial = 0
+// console.log(initial);
+// initial = 20
+// console.log(initial);
+
+const val = 0
+console.log(val);
+val = 10
