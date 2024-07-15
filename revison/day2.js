4796a39a3fde74891f4e3fe06b038971bf4644e3
Priyanshu Rawat <167407828+priyanshurawat1@users.noreply.github.com>
2024-07-15 05:24:21 +0000

    day2

---
+++ revison/day2.js
@@ -0,0 +1,25 @@
+let a ='4',b = 4
+let c = '4'
+// console.log(a%b);
+
+// a += 5
+// console.log(a);
+// if(a == b){
+//     console.log('equal');
+// }
+// if(b === c){
+//     console.log('and of same type');
+// }
+
+// if(a == b && b===c){
+//     console.log('equal');
+// }
+// if(a == b || b===c){
+//     console.log('equal');
+// }
+// if(!(a == b)){
+//     console.log('not');
+// }
+
+const num = -7
+num > 0 ? console.log('Positive') : console.log('Negative');
